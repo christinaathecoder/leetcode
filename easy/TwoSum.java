f7dab41de614c389db1bd227b3aa7c06f5ebf62f
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:29:29 -0800

    Create TwoSum.java

---
+++ easy/TwoSum.java
@@ -0,0 +1,15 @@
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++)   {
+            if (numMap.containsKey(nums[i]))    {
+                int[] newNums = {numMap.get(nums[i]), i};
+                return newNums;
+            }
+            else    {
+                numMap.put(target - nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
