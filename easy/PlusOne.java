4b600c633eab147d9b850069c84d05aef78e468a
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:40:04 -0800

    Create PlusOne.java

---
+++ easy/PlusOne.java
@@ -0,0 +1,38 @@
+class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length - 1;
+        int count = 0;
+        if (digits[len] != 9)   {
+            digits[len] = (digits[len] + 1);
+            return digits;
+        }
+        else if (digits[len] == 9)    {
+            int[] r = new int[digits.length + 1];
+            r[0] = 1;
+            for (int i = 0; i <= len; i++)   {
+                if (digits[i] == 9) {
+                    count++;
+                }
+                if (count == digits.length) {
+                    for (int j = 1; j < r.length; j++)  {
+                        r[j] = 0;
+                    }
+                    return r;
+                }
+                if (count != digits.length && i == len) {
+                    for (int k = len; k >= 0; k--)  {
+                        if (digits[len] == 9 || digits[k] == 10) {
+                            digits[k] = 0;
+                            digits[k - 1] = digits[k - 1] + 1;
+                        }
+                        else    {
+                            break;
+                        }
+                    }
+                    return digits;
+                }
+            }
+        }
+        return digits;
+    }
+}
