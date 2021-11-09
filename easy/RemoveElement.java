0e10769073a959f710a973ea2713a88b330537b1
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:37:02 -0800

    Create RemoveElement.java

---
+++ easy/RemoveElement.java
@@ -0,0 +1,14 @@
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int a = 0;
+        int b = 0;
+        while (a < nums.length) {
+            if (nums[a] != val) {
+                nums[b] = nums[a];
+                b++;
+            }
+            a++;
+        }
+        return b;
+    }
+}
