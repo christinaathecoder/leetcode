ee3fcc1e01b29555e29d8c0eb0160a6d435727c9
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:35:13 -0800

    Create MergeSortedList.java

---
+++ easy/MergeSortedList.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeSortedList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode start = new ListNode(0);
+        
+        ListNode n = start;
+        ListNode n1 = l1;
+        ListNode n2 = l2;
+        while (n1 != null && n2 != null)    {
+            if (n1.val < n2.val)    {
+                n.next = n1;
+                n1 = n1.next;
+            }
+            else    {
+                n.next = n2;
+                n2 = n2.next;
+            }
+            n = n.next;
+        }
+        if (n1 != null) {
+            n.next = n1;
+        }
+        if (n2 != null) {
+            n.next = n2;
+        }
+        return start.next;
+    }
+}
