5da944ca343facce85410d009a54b2751b2d9942
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:36:05 -0800

    Create RemoveDupsSortedArr.java

---
+++ easy/RemoveDupsSortedArr.java
@@ -0,0 +1,12 @@
+class RemoveDupsSortedArr {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++)   {
+            if (nums[k] != nums[i]) {
+                k++;
+                nums[k] = nums[i];
+            }
+        }
+        return k + 1;
+    }
+}
