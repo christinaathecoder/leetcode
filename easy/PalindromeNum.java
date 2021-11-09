4f2827a5643632d9f79a17f9552ad4afebe76269
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:30:31 -0800

    Create PalindromeNum.java

---
+++ easy/PalindromeNum.java
@@ -0,0 +1,22 @@
+class PalindromeNum {
+    public boolean isPalindrome(int x) {
+        int reverse = 0;
+        int i= 0;
+        int p = x;
+        if (x < 0)  {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+        while (x != 0)  {
+            i = x % 10;
+            reverse = reverse * 10 + i;
+            x = x/10;
+            if (reverse == p)   {
+                return true;
+            }
+        }
+        return false;
+    }
+}
