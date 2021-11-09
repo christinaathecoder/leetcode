6d834c9c86a6ed666fd6e76e167d7182301dcce4
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:39:20 -0800

    Create LenOfLastWord.java

---
+++ easy/LenOfLastWord.java
@@ -0,0 +1,7 @@
+class LenOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.trim().split("\\s");
+        int end = arr.length - 1;
+        return arr[end].length();   
+    }
+}
