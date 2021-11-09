bf20a7d1a455e554756ffb85cf763d7b2e38ada5
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:34:21 -0800

    Create ValidParentheses.java

---
+++ easy/ValidParentheses.java
@@ -0,0 +1,26 @@
+class ValidParentheses {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++)    {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            }
+            else if (map.containsValue(c))  {
+                if (!stack.empty() && map.get(stack.peek()) == c)   {
+                    stack.pop();
+                }
+                else    {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
