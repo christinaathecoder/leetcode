9e42dc57b2df57ca8b7aa8b8177dc1c97cd0da42
christinaadanks <christinaa.danks@me.com>
2021-11-08 18:41:28 -0800

    Create Sqrt.java

---
+++ easy/Sqrt.java
@@ -0,0 +1,15 @@
+class Sqrt {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        double s = (double)x/2;
+        double n;
+        do  {
+            n = s;
+            s = (s + ((double)x/s))/2;
+        }
+        while   ((n - s) != 0);
+        return (int)s;
+    }
+}
