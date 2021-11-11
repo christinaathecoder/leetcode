c430e3e606b7b7e73f7a1e5768a5039dbf0d545f
christinaadanks <christinaa.danks@me.com>
2021-11-10 16:07:55 -0800

    Update LCP.java

--- easy/LCP/LCP.java
+++ easy/LCP/LCP.java
@@ -2,11 +2,16 @@ class LCP {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder prefix = new StringBuilder("");
         int len = strs.length;
+        
+        //sort array first
         Arrays.sort(strs);
-        if (strs.length == 0)   {
+        if (strs.length == 0)   { //if empty, return string
             return prefix.toString();
         }
+        
+        //find minimum string length
         int min = Math.min(strs[0].length(), strs[len - 1].length());
+        
         int i = 0;
         while (i < min && strs[0].charAt(i) == strs[len - 1].charAt(i)) {
             prefix.append(strs[0].charAt(i));
