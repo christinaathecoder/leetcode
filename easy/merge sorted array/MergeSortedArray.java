6f751a6cb84df6b7fc5a18a82b6356f002cdd706
christinaadanks <christinaa.danks@me.com>
2021-11-13 00:03:33 -0800

    comments added

--- easy/merge sorted array/MergeSortedArray.java
+++ easy/merge sorted array/MergeSortedArray.java
@@ -1,26 +1,28 @@
 package com.company;
 
 /**
- * @author christinaadanks 
+ * @author christinaadanks
  */
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int len = m + n - 1; //total len of merged array
-        int k = m - 1;
-        int p = n - 1;
+        int k = m - 1; //end of sorted nums1 array
+        int p = n - 1; //end of sorted nums2 array
 
         while (len >= 0 && p >= 0)    {
+            //input int moving backwards - if nums1 contains larger int
             if (k >= 0 && nums1[k] >= nums2[p])    {
                 nums1[len] = nums1[k];
-                nums1[k] = 0;
-                k--;
-                len--;
+                nums1[k] = 0; //remove (reset to 0)
+                k--; //decrease position for comparison
+                len--; //decrease position of insertion
             }
+            //input int moving backwards - if nums2 contains larger int
             else if (k < 0 || nums1[k] < nums2[p])  {
                 nums1[len] = nums2[p];
-                p--;
-                len--;
+                p--; //decrease position for comparison
+                len--; //decrease position of insertion
             }
             else    {
                 break;
